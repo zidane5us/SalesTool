20da47263a862f31feae5b640a583ab81b8637f8
Aron Wozniak <Aron Wozniak@9.83.43.192>
2015-03-19 12:06:46 +0100

    Add a simple output change.

--- SalesTool/src/ca/demo/salestool/Main.java
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -11,5 +11,7 @@ public class Main {
 	private static void displayGreeting() {
 		System.out.println("Hello happy sales people!");
 		System.out.println("This app shows sales data");
+		
+		System.out.println("Test 1");
 	}
 }
